8dce18a14fe7702e2f4fdbbca83e6f8f0dcadb7d
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-31 13:42:36 +0100

    commit 2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,37 @@
 <template>
-    <div>
-        <!-- footer -->
+	<div>
+		<!-- footer -->
 		<div class="bg-gray-700 h-50">
 			<div
 				class="flex sm:justify-between items-center flex-col sm:flex-row max-w-full mx-auto p-16 text-white "
 			>
 				<div class="sm:mt-6 mt-3">
 					<img
-						class="sm:h-28 sm:w-28 w-32 h-28"
+						class="sm:h-28 sm:w-28 w-20 h-20"
 						src="@/assets/img/logo-white.png"
 						alt="Workflow logo"
 					/>
 				</div>
-				<div class="font-bold flex sm:justify-end text-center flex-col cursor-pointer">
-					<div class="flex justify-end">
+				<div
+					class="font-bold text-lg flex sm:justify-end  flex-col cursor-pointer"
+				>
+					<div class="flex flex-col text-center sm:justify-end">
 						<div class="mt-2">
-							Developed by Raymond Idengeli <br />
-							For LoveToKnowMedia <br />
-							June 2021
-						</div>
+							Developed by Raymond Idengeli
+                        </div>
+							<div>
+								For LoveToKnowMedia
+							</div>
+                            <div>
+                                June 2021
+                            </div>				
 					</div>
-					<div class="mt-6">The Movie Database API</div>
-					<div>
+					<div class="mt-6 flex text-center sm:justify-end">
+                        The Movie Database API
+                    </div>
+					<div class="flex item-center  sm:justify-end">
 						<img
-							class="h-16 w-16 text-center"
+							class="h-16 w-16 "
 							src="@/assets/img/footer-logo.svg"
 							alt="footer logo"
 						/>
@@ -33,5 +41,5 @@
 		</div>
 
 		<!-- footer -->
-    </div>
+	</div>
 </template>
