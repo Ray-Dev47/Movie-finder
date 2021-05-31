3f6db4c8618ff5b87ddf9b870a7b754b865af7ba
Raymond Idengeli <ray.idengeli@gmail.com>
2021-05-31 14:32:30 +0100

    header and footer done width

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -4,17 +4,17 @@
 			<!-- main -->
 			<div class="mt-60 text-center">
 				<div class="text-4xl sm:text-5xl font-bold">MovieFinder</div>
-				<div class="text-lg font-bold mt-4 mb-10 text-gray-600">
+				<div class="text-lg font-bold mt-4 mb-10 text-gray-800">
 					Want to find out about a movie? We've got you!
 				</div>
 				<!-- search movie -->
 				<div>
 					<input
 						type="text"
-						class="form-input text-gray-500 text-5xl font-bold w-10/12 border-custom-orange border-t-0 border-r-0 border-l-0 border-b-4 p-3 outline-none mb-3 placeholder-gray-600"
+						class="form-input text-gray-500 text-2xl sm:text-5xl font-bold w-10/12 border-custom-orange border-t-0 border-r-0 border-l-0 border-b-4 p-3 outline-none mb-3 placeholder-gray-400"
 						placeholder="Search by title... "
 					/>
-					<i class="fas fa-search text-4xl text-gray-500"></i>
+					<i class="fas fa-search text-3xl sm:text-4xl text-gray-400"></i>
 				</div>
 
 				<!-- movie posters -->
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 		<!-- footer -->
 		<div class="bg-gray-700 h-50">
 			<div
-				class="flex sm:justify-between items-center flex-col sm:flex-row max-w-full mx-auto p-16 text-white "
+				class="flex sm:justify-between items-center flex-col sm:flex-row max-w-full mx-auto p-8 text-white "
 			>
 				<div class="sm:mt-6 mt-3">
 					<img
@@ -13,29 +13,30 @@
 					/>
 				</div>
 				<div
-					class="font-bold text-lg flex sm:justify-end  flex-col cursor-pointer"
+					class="font-bold text-lg flex  flex-col cursor-pointer"
 				>
-					<div class="flex flex-col text-center sm:justify-end">
-						<div class="mt-2">
-							Developed by Raymond Idengeli
+					<div class="flex items-center sm:items-end flex-col ">
+						<div class="mt-10 ">
+							Developed by Raymond
                         </div>
-							<div>
+							<div class="">
 								For LoveToKnowMedia
 							</div>
-                            <div>
+                            <div class="text-sm">
                                 June 2021
                             </div>				
 					</div>
-					<div class="mt-6 flex text-center sm:justify-end">
+					<div class="mt-6 text-sm sm:text-lg flex flex-col items-center flex justify-center sm:items-end">
                         The Movie Database API
-                    </div>
-					<div class="flex item-center  sm:justify-end">
+                        <div>
 						<img
 							class="h-16 w-16 "
 							src="@/assets/img/footer-logo.svg"
 							alt="footer logo"
 						/>
 					</div>
+                    </div>
+					
 				</div>
 			</div>
 		</div>
